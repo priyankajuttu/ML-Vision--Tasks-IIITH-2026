4da91c031002c815963f568fbb54235b1b9fe992
priyankajuttu <priyajuttu18@gmail.com>
2026-04-21 19:30:44 +0530

    Update explore_yolo.py

--- Week-04/Task-01-YOLO-Config/explore_yolo.py
+++ Week-04/Task-01-YOLO-Config/explore_yolo.py
@@ -1,10 +1,8 @@
 import yaml
 import os
 
-# ── Use YAML files already in your venv ──
-yaml_path = r"C:\ML-Internship\venv\Lib\site-packages\ultralytics\cfg\datasets\coco128.yaml"
 
-# Fix encoding error with utf-8
+yaml_path = r"C:\ML-Internship\venv\Lib\site-packages\ultralytics\cfg\datasets\coco128.yaml"
 with open(yaml_path, 'r', encoding='utf-8') as f:
     data = yaml.safe_load(f)
 
@@ -19,7 +17,7 @@ print(f"\nAll Class Names:")
 for i, name in enumerate(data.get('names', [])):
     print(f"  {i:3d}: {name}")
 
-# ── Also read YOLO26 model yaml ──
+
 model_yaml = r"C:\ML-Internship\venv\Lib\site-packages\ultralytics\cfg\models\26\yolo26.yaml"
 
 print("\n" + "=" * 50)
